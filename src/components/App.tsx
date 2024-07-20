65a503e95fe65f9153fef10fdb7cc966ec38ad09
Eric Tran <erictran1547@gmail.com>
2024-07-20 16:30:59 -0700

    Working fetch from API and working custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,47 @@
+import Header, { HeaderTop } from "./Header";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import { useEffect, useState } from "react";
+
 import Background from "./Background";
+import BookmarksButton from "./BookmarksButton";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import JobItemContent from "./JobItemContent";
+import JobList from "./JobList";
+import Logo from "./Logo";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SearchForm from "./SearchForm";
+import SortingControls from "./SortingControls";
+import { useJobItems } from "../lib/hooks";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, isLoading] = useJobItems(searchText);
+
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobList jobItems={jobItems} isLoading={isLoading} />
+
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
       <Footer />
     </>
   );
